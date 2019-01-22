a5161c77dad0349e3b4d3af40f17e02d0e1c4a8a
klogpok <kloleg@gmail.com>
2019-01-22 20:24:48 +0200

    Implement Context to StatusBar component

--- source/components/StatusBar/index.js
+++ source/components/StatusBar/index.js
@@ -1,22 +1,28 @@
 // Core
 import React, { Component } from 'react';
 
+// Components
+import { Consumer } from '../HOC/withProfile';
+
 // Instruments
 import Styles from './styles.m.css';
-import avatar from '../../theme/assets/bob.png';
 
 export default class index extends Component {
     render() {
         return (
-            <section className = { Styles.StatusBar }>
-                <button>
-                    <img src = { avatar } />
-                    <span>Bob</span>
-                    &nbsp;
-                    <span>Sponge</span>
-                </button>
+            <Consumer>
+                {(context) => (
+                    <section className = { Styles.StatusBar }>
+                        <button>
+                            <img src = { context.avatar } />
+                            <span>{context.currentUserFirstName}</span>
+                            &nbsp;
+                            <span>{context.currentUserLastName}</span>
+                        </button>
 
-            </section>
+                    </section>
+                )}
+            </Consumer>
         );
     }
 }
